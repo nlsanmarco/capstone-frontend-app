b6abcadde7742bce6e60223b15809a06acc35d96
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-10-29 10:57:35 -0400

    adds signup page and routes

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link>
       |
+      <router-link to="/signup">Signup</router-link>
+      |
       <router-link :to="`/users/${getUserId()}/edit`">Edit Profile</router-link>
     </div>
     <router-view />
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Signup from "../views/Signup.vue";
 import UsersEdit from "../views/UsersEdit.vue";
 
 Vue.use(VueRouter);
@@ -11,6 +12,13 @@ const routes = [
     name: "Home",
     component: Home,
   },
+
+  {
+    path: "/signup",
+    name: "signup",
+    component: Signup,
+  },
+
   {
     path: "/users/:id/edit",
     name: "users-edit",
