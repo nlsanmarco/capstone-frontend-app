2fa4d52e370fd6388dc12d97f545d73a4cdc9ae3
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-10-30 19:59:21 -0400

    adds working fields to favorites index name, breed, age and location

--- src/views/FavoritesIndex.vue
+++ src/views/FavoritesIndex.vue
@@ -2,12 +2,12 @@
   <div class="favorites-index">
     <h1>Your Favorites</h1>
     <div v-for="favorite in favorites" v-bind:key="favorite.id">
-      <h2>Name: {{ favorite.name }}</h2>
-      <h4>Age: {{ favorite.age }}</h4>
-      <h4>Primary Breed: {{ favorite.primary_breed }}</h4>
-      <h4>Location: {{ favorite.location }}</h4>
-      <p>images: {{ favorite.primary_photo_cropped }}</p>
-      <router-link :to="`/favorites/${favorite.id}`" class="btn btn-outline-secondary">See Details</router-link>
+      <h2>Name: {{ favorite.api_dog.name }}</h2>
+      <h4>Age: {{ favorite.api_dog.age }}</h4>
+      <h4>Primary Breed: {{ favorite.api_dog.breeds.primary }}</h4>
+      <h4>Location: {{ favorite.api_dog.contact.address.city }}, {{ favorite.api_dog.contact.address.state }}</h4>
+      <!-- <p>images: {{ favorite.primary_photo_cropped.small }}</p> -->
+      <!-- <router-link :to="`/api_dogs/${favorite.api_dog_id}`" class="btn btn-outline-secondary">See Details</router-link> -->
     </div>
   </div>
 </template>
