9c17ce8ccecd0cafd33eb0dcd3a3e9abc25f0051
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-10-30 20:01:43 -0400

    fixes breed and location fields on dog index page

--- src/views/DogsIndex.vue
+++ src/views/DogsIndex.vue
@@ -4,9 +4,9 @@
     <div v-for="api_dog in api_dogs" v-bind:key="api_dog.id">
       <h2>Name: {{ api_dog.name }}</h2>
       <h4>Age: {{ api_dog.age }}</h4>
-      <h4>Primary Breed: {{ api_dog.primary_breed }}</h4>
-      <h4>Location: {{ api_dog.location }}</h4>
-      <p>images: {{ api_dog.primary_photo_cropped }}</p>
+      <h4>Primary Breed: {{ api_dog.breeds.primary }}</h4>
+      <h4>Location: {{ api_dog.contact.address.city }}, {{ api_dog.contact.address.state }}</h4>
+      <p>images: {{ api_dog.primary_photo_cropped.small }}</p>
       <router-link :to="`/api_dogs/${api_dog.id}`" class="btn btn-outline-secondary">See Details</router-link>
     </div>
   </div>
