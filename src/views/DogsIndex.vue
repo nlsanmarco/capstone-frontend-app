ed0752d47f9be879b7cb89e46a9fa4b6c2d43104
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-11-01 19:27:09 -0400

    removes dogs that do not have a profile pic from dogs index, adds secondary breed to description on dogs index onyl when available

--- src/views/DogsIndex.vue
+++ src/views/DogsIndex.vue
@@ -2,11 +2,14 @@
   <div class="dogs-index">
     <h1>Your Matches</h1>
     <div v-for="api_dog in api_dogs" v-bind:key="api_dog.id">
-      <h2>Name: {{ api_dog.name }}</h2>
-      <h4>Age: {{ api_dog.age }}</h4>
-      <h4>Primary Breed: {{ api_dog.breeds.primary }}</h4>
-      <h4>Location: {{ api_dog.contact.address.city }}, {{ api_dog.contact.address.state }}</h4>
-      <router-link :to="`/api_dogs/${api_dog.id}`"><img :src="api_dog.primary_photo_cropped.small" /></router-link>
+      <div v-if="api_dog.primary_photo_cropped !== null">
+        <h2>Name: {{ api_dog.name }}</h2>
+        <h4>Age: {{ api_dog.age }}</h4>
+        <h4>Primary Breed: {{ api_dog.breeds.primary }}</h4>
+        <h4 v-if="api_dog.breeds.secondary !== null">Secondary Breed: {{ api_dog.breeds.secondary }}</h4>
+        <h4>Location: {{ api_dog.contact.address.city }}, {{ api_dog.contact.address.state }}</h4>
+        <router-link :to="`/api_dogs/${api_dog.id}`"><img :src="api_dog.primary_photo_cropped.small" /></router-link>
+      </div>
     </div>
   </div>
 </template>
