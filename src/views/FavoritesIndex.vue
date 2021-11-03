90653db945c48946e8aed2b2a0b944b3c7922744
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-11-03 12:17:42 -0400

    adds info fields and route to dog show page by clicking on image

--- src/views/FavoritesIndex.vue
+++ src/views/FavoritesIndex.vue
@@ -5,10 +5,13 @@
       <h2>Name: {{ favorite.api_dog.name }}</h2>
       <h4>Age: {{ favorite.api_dog.age }}</h4>
       <h4>Primary Breed: {{ favorite.api_dog.breeds.primary }}</h4>
+      <h4 v-if="favorite.api_dog.breeds.secondary !== null">
+        Secondary Breed: {{ favorite.api_dog.breeds.secondary }}
+      </h4>
       <h4>Location: {{ favorite.api_dog.contact.address.city }}, {{ favorite.api_dog.contact.address.state }}</h4>
-      <img :src="favorite.api_dog.primary_photo_cropped.small" />
-      <!-- <p>images: {{ favorite.primary_photo_cropped.small }}</p> -->
-      <!-- <router-link :to="`/api_dogs/${favorite.api_dog_id}`" class="btn btn-outline-secondary">See Details</router-link> -->
+      <router-link :to="`/api_dogs/${favorite.api_dog.id}`">
+        <img :src="favorite.api_dog.primary_photo_cropped.small" />
+      </router-link>
     </div>
   </div>
 </template>
