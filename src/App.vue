0bf3782e1700dca2bdb9543b20c7822e0f3db209
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-11-06 21:36:51 -0400

    adds styled navbar

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,7 @@
 <template>
   <div id="app">
     <!-- ======== @Region: #header ======== -->
-    <div id="header">
-      <!--Header upper region-->
-      <div class="header-upper">
-        <!-- all direct children of the .header-inner element will be vertically aligned with each other you can override all the behaviours using the flexbox utilities (flexbox.htm) All elements with .header-brand & .header-block-flex wrappers will automatically be aligned inline & vertically using flexbox, this can be overridden using the flexbox utilities (flexbox.htm) Use .header-block to stack elements within on small screen & "float" on larger screens use .order-first or/and .order-last classes to make an element show first or last within .header-inner or .headr-block elements -->
-        <div class="header-inner container">
-          <!--user menu-->
-          <div class="header-block-flex order-1 mr-auto">
-            <nav class="nav nav-sm header-block-flex">
-              <a class="nav-link d-md-none" href="login.html"><i class="fa fa-user"></i></a>
-              <a class="nav-link text-xs text-uppercase d-none d-md-block" href="#signup-modal" data-toggle="modal">
-                Sign Up
-              </a>
-              <a class="nav-link text-xs text-uppercase d-none d-md-block" href="#login-modal" data-toggle="modal">
-                Login
-              </a>
-            </nav>
-            <div class="header-divider header-divider-sm"></div>
-            <!--language menu-->
-            <div class="dropdown dropdowns-no-carets">
-              <a href="#en" class="nav-link text-xs p-0 dropdown-toggle ml-1" data-toggle="dropdown">
-                <i class="flag-icon flag-icon-gb"></i>
-              </a>
-              <div class="dropdown-menu dropdown-menu-sm rounded dropdown-menu-arrow border-w-2 ml-2-neg">
-                <a href="#es" class="dropdown-item lang-es">
-                  <i class="flag-icon flag-icon-es"></i>
-                  Spanish
-                </a>
-                <a href="#pt" class="dropdown-item lang-pt">
-                  <i class="flag-icon flag-icon-pt"></i>
-                  Portguese
-                </a>
-                <a href="#cn" class="dropdown-item lang-cn">
-                  <i class="flag-icon flag-icon-cn"></i>
-                  Chinese
-                </a>
-                <a href="#se" class="dropdown-item lang-se">
-                  <i class="flag-icon flag-icon-se"></i>
-                  Swedish
-                </a>
-              </div>
-            </div>
-          </div>
-          <!--social media icons-->
-          <div class="nav nav-icons header-block order-12">
-            <!--@todo: replace with company social media details-->
-            <a href="#" class="nav-link">
-              <i class="fab fa-twitter-square icon-1x"></i>
-              <span class="sr-only">Twitter</span>
-            </a>
-            <a href="#" class="nav-link">
-              <i class="fab fa-facebook-square icon-1x"></i>
-              <span class="sr-only">Facebook</span>
-            </a>
-            <a href="#" class="nav-link">
-              <i class="fab fa-linkedin icon-1x"></i>
-              <span class="sr-only">Linkedin</span>
-            </a>
-            <a href="#" class="nav-link">
-              <i class="fab fa-google-plus-square icon-1x"></i>
-              <span class="sr-only">Google plus</span>
-            </a>
-          </div>
-        </div>
-      </div>
+    <div id="header" class="page page-index navbar-layout-default">
       <div data-toggle="sticky">
         <!--Header search region - hidden by default -->
         <div class="header-search collapse" id="search">
@@ -109,17 +46,6 @@
               >
                 <i class="fa fa-bars"></i>
               </a>
-              <!--Show/hide trigger for #offcanvas-sidebar -->
-              <a
-                href="#"
-                title="Click me you'll get a surprise"
-                class="btn btn-icon btn-link header-btn float-right order-last"
-                data-toggle="off-canvas"
-                data-target="#offcanvas-sidebar"
-                data-settings='{"cloneTarget":false}'
-              >
-                <i class="ion-android-more-vertical"></i>
-              </a>
             </div>
 
             <div class="navbar navbar-expand-md navbar-static-top">
@@ -127,59 +53,98 @@
               <div class="navbar-main collapse">
                 <!--main navigation-->
                 <ul class="nav navbar-nav navbar-nav-stretch float-lg-right dropdown-effect-fade">
-                  <!-- Pages -->
                   <li class="nav-item dropdown">
-                    <router-link to="/">Home</router-link>
+                    <router-link
+                      class="nav-link dropdown-toggle"
+                      id="pages-drop"
+                      data-toggle="dropdown"
+                      data-hover="dropdown"
+                      to="/"
+                    >
+                      Home
+                    </router-link>
                   </li>
                   <li class="nav-item dropdown" v-if="!isLoggedIn()">
-                    <router-link to="/signup">Signup</router-link>
+                    <router-link
+                      class="nav-link dropdown-toggle"
+                      id="pages-drop"
+                      data-toggle="dropdown"
+                      data-hover="dropdown"
+                      to="/signup"
+                    >
+                      Signup
+                    </router-link>
                   </li>
                   <li class="nav-item dropdown" v-if="!isLoggedIn()">
-                    <router-link to="/login">Login</router-link>
+                    <router-link
+                      class="nav-link dropdown-toggle"
+                      id="pages-drop"
+                      data-toggle="dropdown"
+                      data-hover="dropdown"
+                      to="/login"
+                    >
+                      Login
+                    </router-link>
                   </li>
                   <li class="nav-item dropdown" v-if="isLoggedIn()">
-                    <router-link to="/logout">Logout</router-link>
+                    <router-link
+                      class="nav-link dropdown-toggle"
+                      id="pages-drop"
+                      data-toggle="dropdown"
+                      data-hover="dropdown"
+                      to="/logout"
+                    >
+                      Logout
+                    </router-link>
                   </li>
                   <li class="nav-item dropdown">
-                    <router-link :to="`/users/${getUserId()}/edit`">Edit Profile</router-link>
+                    <router-link
+                      class="nav-link dropdown-toggle"
+                      id="pages-drop"
+                      data-toggle="dropdown"
+                      data-hover="dropdown"
+                      :to="`/users/${getUserId()}/edit`"
+                    >
+                      Edit Profile
+                    </router-link>
                   </li>
                   <li class="nav-item dropdown">
-                    <router-link to="/api_dogs">Dog Matches</router-link>
+                    <router-link
+                      class="nav-link dropdown-toggle"
+                      id="pages-drop"
+                      data-toggle="dropdown"
+                      data-hover="dropdown"
+                      to="/api_dogs"
+                    >
+                      Dog Matches
+                    </router-link>
                   </li>
                   <li class="nav-item dropdown">
-                    <router-link to="/favorites">Favorites</router-link>
+                    <router-link
+                      class="nav-link dropdown-toggle"
+                      id="pages-drop"
+                      data-toggle="dropdown"
+                      data-hover="dropdown"
+                      to="/favorites"
+                    >
+                      Favorites
+                    </router-link>
                   </li>
                 </ul>
               </div>
               <!--/.navbar-collapse -->
+              <!--
+                 <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle">
+                      <router-link to="/favorites">Favorites</router-link>
+                    </a>
+                  </li> -->
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div id="nav">
-      <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li v-if="!isLoggedIn()">
-          <router-link to="/signup">Signup</router-link>
-        </li>
-        <li v-if="!isLoggedIn()">
-          <router-link to="/login">Login</router-link>
-        </li>
-        <li v-if="isLoggedIn()">
-          <router-link to="/logout">Logout</router-link>
-        </li>
-        <li>
-          <router-link :to="`/users/${getUserId()}/edit`">Edit Profile</router-link>
-        </li>
-        <li>
-          <router-link to="/api_dogs">Dog Matches</router-link>
-        </li>
-        <li>
-          <router-link to="/favorites">Favorites</router-link>
-        </li>
-      </ul>
-    </div>
+
     <div v-if="flashMessage">
       {{ flashMessage }}
       <button v-on:click="flashMessage = ''">Close</button>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -36,7 +36,7 @@ export default {
           axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
           localStorage.setItem("jwt", response.data.jwt);
           localStorage.setItem("user_id", response.data.user_id);
-          this.$parent.flashMessage = "Successfully logged in!";
+          // this.$parent.flashMessage = "Successfully logged in!";
           this.$router.push(`/users/${response.data.user_id}/edit`);
         })
         .catch((error) => {
