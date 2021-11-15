f039f34a24525f9808dc71543e753721ffbb1b8c
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-11-15 18:13:30 -0500

    removes / from end of heroku link

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://tn-dog-backend.herokuapp.com/";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://tn-dog-backend.herokuapp.com";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
